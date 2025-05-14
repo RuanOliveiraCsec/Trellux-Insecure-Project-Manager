26cc5678210eef2a592359656b1c1929c19bc8d6
Ruan Oliveira <ruanoliveira.csec@gmail.com>
2025-05-14 01:43:52 -0300

    Finalizado o container de Inscrição e adicionado Algumas Animações.

--- front/src/components/ContainerDeInscricao.jsx
+++ front/src/components/ContainerDeInscricao.jsx
@@ -1,15 +1,17 @@
-import "./css/ContainerDeinscricao.css"
+import "./css/ContainerDeInscricao.css"
 
-function ContainerDeinscricao() {
+function ContainerDeInscricao() {
 
     return (
         <>
-            <div>
-                teste
+            <div id="container-inscricao">
+                <h2 id="container-inscricao__titulo">Reúna, organize e resolva suas tarefas de qualquer lugar.</h2>
+                <p id="container-inscricao__texto">Fuja da bagunça e do caos. Seja mais produtivo com o Trellux.</p>
+                <button id="container-inscricao__botao" aria-label="Inscreva-se no Trellux — é grátis!">Faça a inscrição. É grátis.</button>
             </div>
         </>
     )
 
 }
 
-export default ContainerDeinscricao
+export default ContainerDeInscricao
--- front/src/main.jsx
+++ front/src/main.jsx
@@ -3,10 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './css/Reset.css'
 import './css/Global.css'
 import MenuDaPagina from './components/MenuDaPagina.jsx'
-
+import ContainerDeInscricao from './components/ContainerDeInscricao.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <MenuDaPagina />
+    <ContainerDeInscricao/>
   </StrictMode>
 )
